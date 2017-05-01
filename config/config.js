4a46ef795b93f15d54270479b58fea559c80f42c
ggolfko <5535512134@email.psu.ac.th>
2017-05-02 01:55:54 +0700

    feature / bug fix messages

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "facebook_api_key": "1471850449537860",
-  "facebook_api_secret": "218253dd26511ae9ef1f27453061db1e",
+  "facebook_api_key": "FB_APP_ID",
+  "facebook_api_secret": "FB_API_SECRET",
   "callback_url": "http://localhost:8080/auth/facebook/callback",
 }
